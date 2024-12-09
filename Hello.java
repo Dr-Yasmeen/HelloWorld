07cb9fa02b6841c933f59d909631f4b20e8863b1
Dr.Yasmeen <dr.yasmeen.aashu@gmail.com>
2024-12-09 20:46:02 +0530

    added HelloWorld program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        // For loop to print "Hello, World!" 10 times
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello, World!"+i);
+        }
+    }
+}
